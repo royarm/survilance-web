c7e5bf02bf7f3e954bf256c7e00aa367bd9feb68
Amir Hesam <113293761+Hesam-mhm@users.noreply.github.com>
2024-01-17 21:26:48 +0330

    lhlhk

--- htmls/flutter.js
+++ htmls/flutter.js
@@ -133,7 +133,7 @@ _flutter.loader = null;
       const {
         serviceWorkerVersion,
         serviceWorkerUrl = `${baseUri}flutter_service_worker.js?v=${serviceWorkerVersion}`,
-        timeoutMillis = 1,
+        timeoutMillis = 4000,
       } = settings;
 
       // Apply the TrustedTypes policy, if present.
